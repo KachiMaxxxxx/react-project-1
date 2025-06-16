c5ac2bd04c2d84291aeab7a2de2cab97e59b0546
Kachi Maxxxxx <kachimax0101@gmail.com>
2025-06-16 23:12:31 +0100

    joke project updated with .map use

--- react-project-1/src/components/Joke.jsx
+++ react-project-1/src/components/Joke.jsx
@@ -1,19 +1,14 @@
 export default function Joke(props) {
   return (
-    
     <div>
       <article>
         <div className="joke__container">
           <h3 className="joke__setup">{props.setup}</h3>
-          <p className="joke__punchline">{props.punchline}
-          </p>
-            <br />
-            <hr />
+          <p className="joke__punchline">{props.punchline}</p>
+          <br />
+          <hr />
         </div>
       </article>
-  
     </div>
   );
 }
-
-
