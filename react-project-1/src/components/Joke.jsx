91ba5ac9806cbf1ebdd1f96c611eeed97d066fcf
Kachi Maxxxxx <kachimax0101@gmail.com>
2025-06-30 08:31:58 +0100

    conditional rendering update to joke component

--- react-project-1/src/components/Joke.jsx
+++ react-project-1/src/components/Joke.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 export default function Joke(props) {
+  const [isShown, setIsShown] = useState(false);
+  function toggleShown() {
+    setIsShown((prevShown) => !prevShown);
+  }
+  console.log(isShown);
   return (
     <div>
       <article>
         <div className="joke__container">
           <h3 className="joke__setup">{props.setup}</h3>
-          <p className="joke__punchline">{props.punchline}</p>
+          {isShown && <p className="joke__punchline">{props.punchline}</p>}
+          <button className="button" onClick={toggleShown}>
+            Hide/Show
+          </button>
           <br />
           <hr />
         </div>
